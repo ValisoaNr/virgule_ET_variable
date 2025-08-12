b29816f12c5f77ee133c67134dec558d6b8d2bf7
Valisoa <valisoandr12@gmail.com>
2025-08-12 09:51:20 +0300

    Update PointVirgule.h

--- PointVirgule.h
+++ PointVirgule.h
@@ -2,4 +2,20 @@
 
 void testVir(char *fichier);
 int trouveErreur(char *ligne, bool instruction);
+int listerInst#include<stdbool.h>
+
+struct Erreur
+{
+    int tache;
+    int num_ligne;
+};
+typedef struct Erreur Erreur;
+
+void testVir(char *fichier);
+int trouveErreur(char *ligne, bool instruction);
 int listerInst(char *fichier, int *nLigne);
+int trouveFin(char *ligne);
+void supprimeFin(char *fichier, int num_ligne);
+void insererFin(char *fichier, int num_ligne, char caractere);
+void supprimePV(char *fichier, int num_ligne);
+void corrige(char *fichier, Erreur defaut);(char *fichier, int *nLigne);
